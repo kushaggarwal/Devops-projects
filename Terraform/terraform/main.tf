8e93afdf3179c62381a42af7e76580fc2ffcbd04
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-01-13 08:26:58 +0530

    Lambda

--- Terraform/terraform/main.tf
+++ Terraform/terraform/main.tf
@@ -85,5 +85,55 @@ resource "aws_iam_role_policy_attachment" "ProductLambdaRolePolicy" {
   policy_arn = aws_iam_policy.ProductLambdaPolicy.arn
 }
 
+resource "aws_lambda_function" "CreateProductHandler"{
+  function_name = "CreateProductHandler"
+  filename = "../lambda/product_lambda.zip"
+  handler = "createproduct.lambda_handler"
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      REGION = "us-east-1"
+      PRODUCT_TABLE = aws_dynamodb_table.product_table.name
+    }
+  }
+
+  source_code_hash = filebase64sha256("../lambda/product_lambda.zip")
+
+  role = aws_iam_role.ProductLambdaRole.arn
+
+  timeout = "5"
+  memory_size = "128"
+}
+
+resource "aws_api_gateway_integration" "createproduct-lambda" {
+  rest_api_id = aws_api_gateway_rest_api.product_apigw.id
+  resource_id = aws_api_gateway_method.createproduct.resource_id
+  http_method = aws_api_gateway_method.createproduct.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+
+  uri = aws_lambda_function.CreateProductHandler.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw-CreateProductHandler" {
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.CreateProductHandler.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.product_apigw.execution_arn}/*/POST/product"
+}
+
+resource "aws_api_gateway_deployment" "productapistageprod" {
+
+  depends_on = [
+    aws_api_gateway_integration.createproduct-lambda
+  ]  
+
+  rest_api_id = aws_api_gateway_rest_api.product_apigw.id
+  stage_name = "prod"
+}
+
 
 
