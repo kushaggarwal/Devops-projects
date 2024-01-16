8dbf49bbe4f9bb9c8832a2dfd705e256feac6e20
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-01-16 08:13:47 +0530

    variables

--- Terraform/terraform/providers.tf
+++ Terraform/terraform/providers.tf
@@ -7,6 +7,7 @@ terraform {
 }
 
 provider "aws" {
-    profile = "default"
     region = "us-east-1"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
---
+++ Terraform/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  type = string
+  description = "AWS aceess key"
+}
+
+variable "aws_secret_key" {
+  type = string
+  description = "AWS secret key"
+}
